38d1ec819aecf07457e663347520e00cb9cb6111
Pavel <sol.pablo2015@yandex.ru>
2025-09-27 17:25:58 +0300

    make new smart ptr

--- Mafia/Smart_ptr.cpp
+++ Mafia/Smart_ptr.cpp
@@ -18,6 +18,7 @@ private:
     }
 
 public:
+    // Конструкторы
     SmartPtr() : ptr(nullptr), ref_count(nullptr) {}
 
     explicit SmartPtr(T* p) : ptr(p), ref_count(new std::size_t(1)) {}
@@ -40,10 +41,12 @@ public:
         release();
     }
 
+    // Разыменование
     T& operator*() const { return *ptr; }
     T* operator->() const { return ptr; }
     T* get() const { return ptr; }
 
+    // Методы
     void reset(T* p = nullptr) {
         release();
         if (p) {
@@ -60,6 +63,21 @@ public:
         std::swap(ref_count, other.ref_count);
     }
 
+    std::size_t use_count() const {
+        return ref_count ? *ref_count : 0;
+    }
+
+    // Сравнение
     bool operator==(const SmartPtr& other) const { return ptr == other.ptr; }
     bool operator!=(const SmartPtr& other) const { return ptr != other.ptr; }
+    bool operator<(const SmartPtr& other) const { return ptr < other.ptr; }
+    bool operator>(const SmartPtr& other) const { return ptr > other.ptr; }
+    bool operator<=(const SmartPtr& other) const { return ptr <= other.ptr; }
+    bool operator>=(const SmartPtr& other) const { return ptr >= other.ptr; }
 };
+
+// Фабричная функция
+template<typename T, typename... Args>
+SmartPtr<T> make_smart_ptr(Args&&... args) {
+    return SmartPtr<T>(new T(std::forward<Args>(args)...));
+}
--- Mafia/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Game.hpp"
-
-int main() {
-    int N = 6; // количество игроков
-    Game g(N);
-    g.start();
-    return 0;
-}
